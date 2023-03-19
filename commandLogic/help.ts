9e78b6a37fb37c4743e0408d7d668baf2ead7c62
OwenPendrighElliott <owenpendrighelliott@gmail.com>
2023-03-19 12:24:29 +1100

    updated help

--- commandLogic/help.ts
+++ commandLogic/help.ts
@@ -7,9 +7,12 @@ Available commands:
     open - Opens a .lnk or .txt file (read-only)
     serve - Serve a given '.page' file
     clear - Clear the terminal
+    useradd - Adds a new user
+    logout - logs you out
 
 Navigation:
     Use the up and down arrows to cycle through previous commands.
+    Press the tab key to autocomplete the name of a folder or file in the current directory
 `;
 
 export default helpStr;
